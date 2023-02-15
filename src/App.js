f4777e543edf2cfe5c69ba8088ec7e880b0987b7
Mehrnaz Aghamohammadhasani <mehrnaz.mohammadhasani@gmail.com>
2023-02-15 11:42:22 +0100

    finish project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => setUsers(data))
+      .catch(error => console.log(error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>User Addresses</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            <h2>{user.name}</h2>
+            <p>{user.address.street}</p>
+            <p>{user.address.suite}</p>
+            <p>{user.address.city}</p>
+            <p>{user.address.zipcode}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
